4c35bd482fc042b97cdc2197cbe4b88a05da2d5d
Sasha <ttsvan@msn.com>
2016-04-05 23:07:31 -0700

    Adds more functions to Block class for updating and providing getters.

--- Ellio/src/com/sasha/game/model/Block.java
+++ Ellio/src/com/sasha/game/model/Block.java
@@ -20,4 +20,49 @@ public class Block {
 		rect = new Rectangle((int) x, (int) y, width, height);
 		visible = false;
 	}
+	
+	// Note: Velocity value will be passed in from PlayState!
+	public void update(float delta, float velX) {
+		x += velX * delta;
+		if (x <= -50) {
+			reset();
+		}
+		updateRect();
+	}
+	
+	public void updateRect() {
+		rect.setBounds((int) x, (int) y, width, height);
+	}
+	
+	public void reset() {
+		visible = true;
+		// 1 in 3 change of becoming upper block
+		if (RandomNumberGenerator.getRandInt(3) == 0) {
+			y = UPPER_Y;
+		} else {
+			y = LOWER_Y;
+		}
+		x += 1000;
+	}
+	
+	public void onCollide(Player p) {
+		visible = false;
+		p.pushBack(30);
+	}
+	
+	public float getX() {
+		return x;
+	}
+	
+	public float getY() {
+		return y;
+	}
+	
+	public boolean isVisible() {
+		return visible;
+	}
+	
+	public Rectangle getRect() {
+		return rect;
+	}
 }
