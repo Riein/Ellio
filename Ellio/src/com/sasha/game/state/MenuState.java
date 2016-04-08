a06fffe5314d01b8b4b14465ae8853baccd81e5c
Sasha <ttsvan@msn.com>
2016-04-07 22:13:29 -0700

    Adds GameOverState for game ending, MenuState for menu interaction and PlayState to handle game interactions.

--- Ellio/src/com/sasha/game/state/MenuState.java
+++ Ellio/src/com/sasha/game/state/MenuState.java
@@ -5,37 +5,55 @@ import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 
 import com.sasha.game.main.Resources;
+import com.sasha.game.main.GameMain;
 
 public class MenuState extends State{
-
+	private int currentSelection = 0;
+	
 	@Override
 	public void init() {
-		System.out.println("Entered MenuState");
+		// Do Nothing
 	}
 
 	@Override
 	public void update(float delta) {
-	
+		// Do Nothing
 	}
 
 	@Override
 	public void render(Graphics g) {
 		g.drawImage(Resources.welcome, 0, 0, null);
+		if (currentSelection == 0) {
+			g.drawImage(Resources.selector, 335, 241, null);
+		} else {
+			g.drawImage(Resources.selector, 335, 291, null);
+		}
 	}
 
 	@Override
 	public void onClick(MouseEvent e) {
-		
+		// Do Nothing
 	}
 
 	@Override
 	public void onKeyPress(KeyEvent e) {
-		
+		int key = e.getKeyCode();
+		if (key == KeyEvent.VK_SPACE || key == KeyEvent.VK_ENTER) {
+			if (currentSelection == 0) {
+				setCurrentState(new PlayState());
+			} else if(currentSelection == 1) {
+				GameMain.sGame.exit();
+			}
+		} else if (key == KeyEvent.VK_UP) {
+			currentSelection = 0;
+		} else if (key == KeyEvent.VK_DOWN) {
+			currentSelection = 1;
+		}
 	}
 
 	@Override
 	public void onKeyRelease(KeyEvent e) {
-		
+		// Do Nothing
 	}
 
 }
